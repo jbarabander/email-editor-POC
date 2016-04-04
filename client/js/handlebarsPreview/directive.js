9dfc43b09f93038de8cf785011dd15996d573254
Justin Barabander <jbarabander@gmail.com>
2016-04-04 11:53:30 -0400

    Added in watch collection

--- client/js/handlebarsPreview/directive.js
+++ client/js/handlebarsPreview/directive.js
@@ -1,32 +1,38 @@
-(function() {
-	'use strict';
-	angular.module('handlebarsTextangular')
-	.directive('handlebarsPreview', function() {
-		return {
-			restrict: 'E',
-			templateUrl: './js/handlebarsPreview/tpl.html',
-			controller: HandlebarsPreviewController,
-			controllerAs: 'HandlebarsPreviewController'
-		}
-	})
+(function () {
+    'use strict';
+    angular.module('handlebarsTextangular')
+        .directive('handlebarsPreview', function () {
+            return {
+                restrict: 'E',
+                templateUrl: './js/handlebarsPreview/tpl.html',
+                controller: HandlebarsPreviewController,
+                controllerAs: 'HandlebarsPreviewController'
+            }
+        })
 
-	function HandlebarsPreviewController($sanitize) {
-		var vm = this;
-		vm.emailHtml = '';
-		vm.context = '{}';
-		vm.compileTemplate = function() {
-			var parsedContext;
-			try {
-				parsedContext = JSON.parse(vm.context);
-			} catch(err) {
-				return "There was an error with the context passed to the template";
-			}
-			try {
-				vm.compiledTemplate = Handlebars.compile(vm.emailHtml)(parsedContext);
-				return vm.compiledTemplate;
-			} catch(err) {
-				return '';
-			}
-		}
-	}
+    function HandlebarsPreviewController($scope) {
+        var vm = this;
+        vm.emailHtml = '';
+        vm.context = '{}';
+        vm.compileTemplate = function () {
+            var parsedContext;
+            try {
+                parsedContext = JSON.parse(vm.context);
+            } catch (err) {
+                return "There was an error with the context passed to the template";
+            }
+            try {
+                vm.compiledTemplate = Handlebars.compile(vm.emailHtml)(parsedContext);
+                return vm.compiledTemplate;
+            } catch (err) {
+                return '';
+            }
+        };
+
+        $scope.$watchCollection(function () {
+                return [vm.emailHtml, vm.context];
+            }, function () {
+                vm.compileTemplate();
+            })
+    }
 })();
